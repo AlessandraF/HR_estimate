b8ad3b147a096d50afaf8558536f4893c6dc1596
Alessandra <alessandra1.fusco@gmail.com>
2015-12-22 15:38:13 +0100

    Update LoadVideo.m

--- LoadVideo.m
+++ LoadVideo.m
@@ -1,31 +1,32 @@
 function y = LoadVideo(video_name)
 
-if exist(video_name),
-    display(['Loading video ' video_name]);
-    vidObj = VideoReader(video_name);
-else
-    display(['Video not found ']);
-end
+    if exist(video_name),
+        display(['Loading video ' video_name]);
+        vidObj = VideoReader(video_name);
+    else
+        display(['Video not found ']);
+    end
 
-n_frames = vidObj.NumberOfFrames;
-frame_rate = vidObj.FrameRate;
+    n_frames = vidObj.NumberOfFrames;
+    frame_rate = vidObj.FrameRate;
 
-format short;
-fprintf('\n Total frames: %d \n', n_frames);
-fprintf('\n Frame rate: %f \n', frame_rate);
+    format short;
+    fprintf('\n Total frames: %d \n', n_frames);
+    fprintf('\n Frame rate: %f \n', frame_rate);
 
-y = zeros(n_frames, 1);
+    y = zeros(n_frames, 1);
 
-for i = 1:n_frames
-    fprintf('\n Processing Frame Number %d/%d', i, n_frames);
-    vidFrame = read(vidObj, i);
-    % Considering only the red channel we obtain:
-    R_channel = vidFrame(:, :, 1);
-    y(i) = sum(sum(R_channel)) / (size(vidFrame, 1) * size(vidFrame, 2));
-    y2(i) = mean( sum(R_channel) / (size(vidFrame, 1) * size(vidFrame, 2)));
-end
+    for i = 1:n_frames
+        frac = i/n_frames;
+        if mod(i,10)==0
+            fprintf('\n Pleas wait... %.00f%% done',100*frac);
+        end
+        vidFrame = read(vidObj, i);
+        R_channel = vidFrame(:, :, 1);                  % Red channel
+        y(i) = sum(sum(R_channel)) / (size(vidFrame, 1) * size(vidFrame, 2));
+    end
 
-fprintf('\n Signal acquired.');
-fprintf('\n You can now run HR_estimate(y, %d)\n', frame_rate);
+    fprintf('\n Video uploaded.');
+    HR_estimate(y, frame_rate);
 
 end
